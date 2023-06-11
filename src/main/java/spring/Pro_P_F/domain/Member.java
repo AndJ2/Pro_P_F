2fb24ec47b61549e4bc2ce7d64a0b98160c0b498
shin_yehee <zia7894@naver.com>
2023-06-11 14:54:19 +0900

    html, css 수정

--- src/main/java/spring/Pro_P_F/domain/Member.java
+++ src/main/java/spring/Pro_P_F/domain/Member.java
@@ -1,12 +1,16 @@
 package spring.Pro_P_F.domain;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter @Setter
 public class Member {
 
     @Id
@@ -23,51 +27,4 @@ public class Member {
 
     private String m_git;
 
-    public String getM_id() {
-        return m_id;
-    }
-
-    public void setM_id(String m_id) {
-        this.m_id = m_id;
-    }
-
-    public String getM_pwd() {
-        return m_pwd;
-    }
-
-    public void setM_pwd(String m_pwd) {
-        this.m_pwd = m_pwd;
-    }
-
-    public String getM_name() {
-        return m_name;
-    }
-
-    public void setM_name(String m_name) {
-        this.m_name = m_name;
-    }
-
-    public String getM_phone() {
-        return m_phone;
-    }
-
-    public void setM_phone(String m_phone) {
-        this.m_phone = m_phone;
-    }
-
-    public String getM_email() {
-        return m_email;
-    }
-
-    public void setM_email(String m_email) {
-        this.m_email = m_email;
-    }
-
-    public String getM_git() {
-        return m_git;
-    }
-
-    public void setM_git(String m_git) {
-        this.m_git = m_git;
-    }
 }
