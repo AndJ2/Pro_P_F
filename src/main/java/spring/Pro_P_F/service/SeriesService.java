84302f823d9bf1bea6c7b502ade2b9e22ce2f03c
shin_yehee <zia7894@naver.com>
2023-09-10 18:33:03 +0900

    시리즈 추가 (사용자 Id도 db에 저장)

--- src/main/java/spring/Pro_P_F/service/SeriesService.java
+++ src/main/java/spring/Pro_P_F/service/SeriesService.java
@@ -2,9 +2,14 @@ package spring.Pro_P_F.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import spring.Pro_P_F.domain.Member;
 import spring.Pro_P_F.domain.Series;
 import spring.Pro_P_F.repository.SeriesRepository;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,13 +19,31 @@ public class SeriesService {
     @Autowired
     private SeriesRepository seriesRepository;
 
+    @Autowired
+    private MemberService memberService;
+
+
+    @Autowired
+    private HttpSession session;
+
     public List<Series> getAllSeries() {
         return seriesRepository.findAll();
     }
 
     public void addSeries(String seriesName) {
+
+        LocalDate currentDate = LocalDate.now();
+
         Series series = new Series();
         series.setS_name(seriesName);
+        series.setS_date(currentDate);
+
+        String mId = (String) session.getAttribute("m_id");
+
+        Member member = memberService.findOne(mId);
+        series.setMember(member);
+
+
         seriesRepository.save(series);
     }
 
